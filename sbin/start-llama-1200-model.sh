6363374be46ee48e2b5cff258385543ed495f909
ychy00001 <yangchunyu00001@163.com>
2023-04-21 00:06:09 +0800

    update llm model shell 1200step

--- sbin/start-llama-1200-model.sh
+++ sbin/start-llama-1200-model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ps aux | grep "fastchat.serve.model_worker" | grep "llama-sft-1200" | grep -v grep | awk '{print $2}' |xargs kill -9
+ps aux | grep "fastchat.serve.model_worker" | grep "1200step" | grep -v grep | awk '{print $2}' |xargs kill -9
 
-nohup python3 -m fastchat.serve.model_worker --model-path /data/model/llama-sft-1200 --host 127.0.0.1 --port 8082 --worker-address http://127.0.0.1:8082 --controller-address http://127.0.0.1:8081 --num-gpus 4 >> /data/project/FastChat/log/model_worker.out 2>&1 &
+nohup python3 -m fastchat.serve.model_worker --model-name "从容V0.1.4" --model-path /data/model/1200step --host 127.0.0.1 --port 8082 --worker-address http://127.0.0.1:8082 --controller-address http://127.0.0.1:8081 --num-gpus 4 >> /data/project/FastChat/log/model_worker.out 2>&1 &
--- sbin/stop-llama-1200-model.sh
+++ sbin/stop-llama-1200-model.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps aux | grep "fastchat.serve.model_worker" | grep "llama-sft-1200" | grep -v grep | awk '{print $2}' |xargs kill -9
+ps aux | grep "fastchat.serve.model_worker" | grep "1200step" | grep -v grep | awk '{print $2}' |xargs kill -9
