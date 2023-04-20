d1a4496eaecf084f25072acd65ec8f072308be8d
ychy00001 <yangchunyu00001@163.com>
2023-04-21 01:37:11 +0800

    add 65B model default prompt

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -99,30 +99,29 @@ conv_one_shot = Conversation(
     messages=(
         ("Human", "What are the key differences between renewable and non-renewable energy sources?"),
         ("Assistant",
-            "Renewable energy sources are those that can be replenished naturally in a relatively "
-            "short amount of time, such as solar, wind, hydro, geothermal, and biomass. "
-            "Non-renewable energy sources, on the other hand, are finite and will eventually be "
-            "depleted, such as coal, oil, and natural gas. Here are some key differences between "
-            "renewable and non-renewable energy sources:\n"
-            "1. Availability: Renewable energy sources are virtually inexhaustible, while non-renewable "
-            "energy sources are finite and will eventually run out.\n"
-            "2. Environmental impact: Renewable energy sources have a much lower environmental impact "
-            "than non-renewable sources, which can lead to air and water pollution, greenhouse gas emissions, "
-            "and other negative effects.\n"
-            "3. Cost: Renewable energy sources can be more expensive to initially set up, but they typically "
-            "have lower operational costs than non-renewable sources.\n"
-            "4. Reliability: Renewable energy sources are often more reliable and can be used in more remote "
-            "locations than non-renewable sources.\n"
-            "5. Flexibility: Renewable energy sources are often more flexible and can be adapted to different "
-            "situations and needs, while non-renewable sources are more rigid and inflexible.\n"
-            "6. Sustainability: Renewable energy sources are more sustainable over the long term, while "
-            "non-renewable sources are not, and their depletion can lead to economic and social instability.")
+         "Renewable energy sources are those that can be replenished naturally in a relatively "
+         "short amount of time, such as solar, wind, hydro, geothermal, and biomass. "
+         "Non-renewable energy sources, on the other hand, are finite and will eventually be "
+         "depleted, such as coal, oil, and natural gas. Here are some key differences between "
+         "renewable and non-renewable energy sources:\n"
+         "1. Availability: Renewable energy sources are virtually inexhaustible, while non-renewable "
+         "energy sources are finite and will eventually run out.\n"
+         "2. Environmental impact: Renewable energy sources have a much lower environmental impact "
+         "than non-renewable sources, which can lead to air and water pollution, greenhouse gas emissions, "
+         "and other negative effects.\n"
+         "3. Cost: Renewable energy sources can be more expensive to initially set up, but they typically "
+         "have lower operational costs than non-renewable sources.\n"
+         "4. Reliability: Renewable energy sources are often more reliable and can be used in more remote "
+         "locations than non-renewable sources.\n"
+         "5. Flexibility: Renewable energy sources are often more flexible and can be adapted to different "
+         "situations and needs, while non-renewable sources are more rigid and inflexible.\n"
+         "6. Sustainability: Renewable energy sources are more sustainable over the long term, while "
+         "non-renewable sources are not, and their depletion can lead to economic and social instability.")
     ),
     offset=2,
     sep_style=SeparatorStyle.SINGLE,
     sep="###",
 )
-
 
 conv_vicuna_v1_1 = Conversation(
     system="A chat between a curious user and an artificial intelligence assistant. "
@@ -134,7 +133,6 @@ conv_vicuna_v1_1 = Conversation(
     sep=" ",
     sep2="</s>",
 )
-
 
 conv_koala_v1 = Conversation(
     system="BEGINNING OF CONVERSATION:",
@@ -157,43 +155,12 @@ conv_dolly = Conversation(
     sep2="### End",
 )
 
-# my_v1 = Conversation(
-#     system="A chat between a curious human and an artificial intelligence assistant. "
-#            "The assistant gives helpful, detailed, and polite answers to the human's questions.",
-#     roles=("Human", "Assistant"),
-#     messages=(
-#         ("Human", "What are the key differences between renewable and non-renewable energy sources?"),
-#         ("Assistant",
-#             "Renewable energy sources are those that can be replenished naturally in a relatively "
-#             "short amount of time, such as solar, wind, hydro, geothermal, and biomass. "
-#             "Non-renewable energy sources, on the other hand, are finite and will eventually be "
-#             "depleted, such as coal, oil, and natural gas. Here are some key differences between "
-#             "renewable and non-renewable energy sources:\n"
-#             "1. Availability: Renewable energy sources are virtually inexhaustible, while non-renewable "
-#             "energy sources are finite and will eventually run out.\n"
-#             "2. Environmental impact: Renewable energy sources have a much lower environmental impact "
-#             "than non-renewable sources, which can lead to air and water pollution, greenhouse gas emissions, "
-#             "and other negative effects.\n"
-#             "3. Cost: Renewable energy sources can be more expensive to initially set up, but they typically "
-#             "have lower operational costs than non-renewable sources.\n"
-#             "4. Reliability: Renewable energy sources are often more reliable and can be used in more remote "
-#             "locations than non-renewable sources.\n"
-#             "5. Flexibility: Renewable energy sources are often more flexible and can be adapted to different "
-#             "situations and needs, while non-renewable sources are more rigid and inflexible.\n"
-#             "6. Sustainability: Renewable energy sources are more sustainable over the long term, while "
-#             "non-renewable sources are not, and their depletion can lead to economic and social instability.")
-#     ),
-#     offset=2,
-#     sep_style=SeparatorStyle.SINGLE,
-#     sep="###",
-# )
-
 conv_templates = {
     "conv_one_shot": conv_one_shot,
     "vicuna_v1.1": conv_vicuna_v1_1,
     "koala_v1": conv_koala_v1,
     "dolly": conv_dolly,
-    # "V0.1.4": my_v1
+    "V0.1.4": conv_dolly
 }
 
 
@@ -204,6 +171,8 @@ def get_default_conv_template(model_name):
     elif "koala" in model_name:
         return conv_koala_v1
     elif "dolly" in model_name:
+        return conv_dolly
+    elif "V0.1.4" in model_name:
         return conv_dolly
     return conv_one_shot
 
