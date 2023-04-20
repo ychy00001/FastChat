a60b28a1d12e73f60eca450c54f1b7f26d3b32cf
ychy00001 <yangchunyu00001@163.com>
2023-04-21 00:31:25 +0800

    update shell default param

--- sbin/start-all.sh
+++ sbin/start-all.sh
@@ -9,4 +9,4 @@ nohup python3 -m fastchat.serve.model_worker --model-path /data/model/llama-7b-h
 # bloom-7b
 #python3 -m fastchat.serve.model_worker --model-path /data/model/bloomz-7b1-mt --host 127.0.0.1 --port 8082 --worker-address http://127.0.0.1:8082 --model-name bloomz-7b1-mt  --controller-address http://127.0.0.1:8081 --num-gpus 4 >> /data/project/FastChat/log/model_worker.out 2>&1 &
 
-nohup python3 -m fastchat.serve.gradio_web_server --share --controller-url http://127.0.0.1:8081 --host 0.0.0.0 --port 8083 --model-list-mode reload >> /data/project/FastChat/log/gradio_web_server.out 2>&1 &
+nohup python3 -m fastchat.serve.gradio_web_server --concurrency-count 30 --controller-url http://127.0.0.1:8081 --host 0.0.0.0 --port 8083 --model-list-mode reload >> /data/project/FastChat/log/gradio_web_server.out 2>&1 &
--- sbin/start-llama-1200-model.sh
+++ sbin/start-llama-1200-model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ps aux | grep "fastchat.serve.model_worker" | grep "1200step" | grep -v grep | awk '{print $2}' |xargs kill -9
 
-nohup python3 -m fastchat.serve.model_worker --model-name "从容V0.1.4" --model-path /data/model/1200step --host 127.0.0.1 --port 8082 --worker-address http://127.0.0.1:8082 --controller-address http://127.0.0.1:8081 --num-gpus 4 >> /data/project/FastChat/log/model_worker.out 2>&1 &
+nohup python3 -m fastchat.serve.model_worker  --max-gpu-memory "75GiB" --limit-model-concurrency 10 --model-name "从容V0.1.4" --model-path /data/model/1200step --host 127.0.0.1 --port 8082 --worker-address http://127.0.0.1:8082 --controller-address http://127.0.0.1:8081 --num-gpus 4 >> /data/project/FastChat/log/model_worker.out 2>&1 &
